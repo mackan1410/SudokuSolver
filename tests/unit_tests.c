46a3c6f90ebfa42cc8684d2c2632634b32d0ee4d
EC2 Default User <ec2-user@ip-172-31-44-187.eu-north-1.compute.internal>
2020-12-10 23:29:21 +0000

    added testcase back

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -53,16 +53,16 @@ void test_solve(){
     
     board_t* board = create_board(cells);
     board_t* board2 = create_board(cells2);
-    //board_t* board3 = create_board(cells3);
+    board_t* board3 = create_board(cells3);
     
     
     CU_ASSERT(solve(board));
     CU_ASSERT(solve(board2));
-    //CU_ASSERT(!solve(board3));
+    CU_ASSERT(!solve(board3));
     
     destroy_board(board);
     destroy_board(board2);
-    //destroy_board(board3);
+    destroy_board(board3);
 }
 
 void test_create_board(){}
